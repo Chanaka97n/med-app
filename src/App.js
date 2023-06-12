7a7fa7a6c4490f08171974159acc13cdbc6a110a
Chanaka97n <chanaka97n@gmail.com>
2023-06-12 19:57:26 +0530

    cleean vode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Router from "./routes";
 
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <div className="">
-      <BrowserRouter>
-        <NavBar />
-        <Router />
-      </BrowserRouter>
-    </div>
-  );
+  return <div className="">test</div>;
 }
 
 export default App;
--- src/components/Layout.js
+++
@@ -1,8 +0,0 @@
-import { Navbar } from "flowbite-react";
-import React from "react";
-
-function Layout() {
-  return <div> </div>;
-}
-
-export default Layout;
